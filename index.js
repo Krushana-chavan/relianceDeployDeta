6fa5fe88349bc58889e1799b6455c440adceefcd
Krushana-chavan <104185649+Krushana-chavan@users.noreply.github.com>
2022-12-15 16:10:19 +0530

    done

--- index.js
+++ index.js
@@ -58,6 +58,57 @@ app.get("/telivison", async (req, res) => {
   }
 });
 
+app.post("/laptop", async (req, res) => {
+  try {
+    const { id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3,} = req.body;
+    let laptop =  new laptopModel({id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3});
+    laptop.save();
+    res.status(200).send(laptop);
+  } catch (e) {
+    res.status(401).send("Invalid api");
+  }
+});
+app.post("/watch", async (req, res) => {
+  try {
+    const { id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3,} = req.body;
+    let watch =  new watchModel({id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3});
+    watch.save();
+    res.status(200).send(watch);
+  } catch (e) {
+    res.status(401).send("Invalid api");
+  }
+});
+app.post("/soundbar", async (req, res) => {
+  try {
+    const { id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3,} = req.body;
+    let sound =  new soundbarModel({id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3});
+    sound.save();
+    res.status(200).send(sound);
+  } catch (e) {
+    res.status(401).send("Invalid api");
+  }
+});
+app.post("/appliance", async (req, res) => {
+  try {
+    const { id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3,} = req.body;
+    let appliance =  new applianceModel({id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3});
+    appliance.save();
+    res.status(200).send(appliance);
+  } catch (e) {
+    res.status(401).send("Invalid api");
+  }
+});
+app.post("/telivison", async (req, res) => {
+  try {
+    const { id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3,} = req.body;
+    let telivision =  new telivisionModel({id, imglink,name,MRP,MRPx,brand,dealprice,dealpricex,discount,discountx,saveprce,saveprcex,cashback,img1,img2,img3});
+    telivision.save();
+    res.status(200).send(telivision);
+  } catch (e) {
+    res.status(401).send("Invalid api");
+  }
+});
+
 app.post("/signup", (req, res) => {
   try {
     const { name, email, role, password, phonenumber } = req.body;
